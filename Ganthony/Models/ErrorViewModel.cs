9c20154ce4ce49ada4b2fae5a557748ab93cc03e
gavin anthony <gavinanthony1@gmail.com>
2019-03-25 18:57:21 -0500

    Add project files.

---
+++ Ganthony/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Ganthony.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
