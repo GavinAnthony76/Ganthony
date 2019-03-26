45d48c6338ed71ea0e76e1baf6ecc60115e29355
gavin anthony <gavinanthony1@gmail.com>
2019-03-25 20:02:21 -0500

    Initial commit. Basic data is on all pages.

--- Ganthony/Controllers/HomeController.cs
+++ Ganthony/Controllers/HomeController.cs
@@ -33,7 +33,11 @@ namespace Ganthony.Controllers
         {
             return View();
         }
-
+        
+        public IActionResult Projects()
+        {
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
