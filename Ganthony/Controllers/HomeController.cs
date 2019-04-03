9b17f93d79fb7c9b9b681db414d719f0e5c47e5b
Gavin Anthony <guidato.llc@gmail.com>
2019-04-03 13:43:01 -0500

    * HomeController.cs: Added resume view
    * Ganthony.csproj:
    * _Layout.cshtml:
    
    * Index.cshtml: add mime type
    
    * Resume.cshtml: added resume cshtml. decided not to use it just yet.
    
    * grayscale.css: modified css throught the site.

--- Ganthony/Controllers/HomeController.cs
+++ Ganthony/Controllers/HomeController.cs
@@ -38,6 +38,10 @@ namespace Ganthony.Controllers
         {
             return View();
         }
+        public IActionResult Resume()
+        {
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
